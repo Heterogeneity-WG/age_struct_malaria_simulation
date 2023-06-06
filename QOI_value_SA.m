38eeb0d2f622fccf353b245f25892983810274fa
Zhuolin Qu <8141673+zhuolinqu@users.noreply.github.com>
2023-06-06 13:16:24 -0500

    SA time series
    updates on the PRCC

--- QOI_value_SA.m
+++ QOI_value_SA.m
@@ -12,14 +12,27 @@ for iQ = 1:length(lQ)
 end
 
 if flag_EE
-    [SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0] = age_structured_Malaria_IC_vac('EE_reset');
-    [SH_solu, EH_solu, DH_solu, AH_solu, VH_solu, UH_solu, SM_solu, EM_solu, IM_solu, Cm_solu, Cac_solu, Cv_solu, Ctot_solu, MH_solu] = ...
-        age_structured_Malaria_vac(P.da,P.na,P.tfinal,SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0);   
-    SH = SH_solu(:,end); EH = EH_solu(:,end); DH = DH_solu(:,end); AH = AH_solu(:,end); MH = MH_solu(:,end);
-    VH = VH_solu(:,end); UH = UH_solu(:,end); Cm = Cm_solu(:,end); Cac = Cac_solu(:,end); Cv = Cv_solu(:,end);
-    Ctot = Ctot_solu(:,end); 
+    [SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0] = age_structured_Malaria_IC_vac('EE_reset');   
+    [t, SH_solu, EH_solu, DH_solu, AH_solu, VH_solu, UH_solu, SM_solu, EM_solu, IM_solu, Cm_solu, Cac_solu, Cv_solu, Ctot_solu, MH_solu] = ...
+        age_structured_Malaria_vac(P.da,P.na, 0, P.tfinal,SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0);   
+    SH = SH_solu(:,time_points); EH = EH_solu(:,time_points); DH = DH_solu(:,time_points); AH = AH_solu(:,time_points); MH = MH_solu(:,time_points);
+    VH = VH_solu(:,time_points); UH = UH_solu(:,time_points); 
+    Cm = Cm_solu(:,time_points); Cac = Cac_solu(:,time_points); Cv = Cv_solu(:,time_points);
+    Ctot = Ctot_solu(:,time_points); 
     PH = SH+EH+DH+AH+VH+UH;
-    SM = SM_solu(:,end); EM = EM_solu(:,end); IM = IM_solu(:,end);
+    SM = SM_solu(:,time_points); EM = EM_solu(:,time_points); IM = IM_solu(:,time_points);
+    % keyboard
+    % figure_setups;
+    % plot(t/365,trapz(SH_solu,1)*da); hold on;
+    % plot(t/365,trapz(EH_solu,1)*da);
+    % plot(t/365,trapz(AH_solu,1)*da);
+    % plot(t/365,trapz(DH_solu,1)*da);
+    % plot(t/365,trapz(VH_solu,1)*da);
+    % plot(t/365,trapz(UH_solu,1)*da);
+    % plot(t/365,trapz(MH_solu,1)*da,'-.r'); % diagnostic
+    % title(['Population size vs time']);
+    % grid on; grid minor
+    % keyboard
 end
 
 Q_val = NaN(length(time_points),length(lQ));
@@ -35,47 +48,41 @@ for iQ = 1:length(lQ)
         case 'RMH'
             [~,~,Q_val(:,iQ)]  = R0_cal();     
         case 'EE-D'
-            Q_val(:,iQ) = trapz(DH)*da;
+            Q_val(:,iQ) = trapz(DH,1)*da;
         case 'EE-D-02-10'
-            Q_val(:,iQ) = trapz(DH(ind0210y))*da;
+            Q_val(:,iQ) = trapz(DH(ind0210y,:),1)*da;
         case 'EE-D-09-24'
-            Q_val(:,iQ) = trapz(DH(ind0924m))*da;
+            Q_val(:,iQ) = trapz(DH(ind0924m,:),1)*da;
         case 'EE-DA'
-            Q_val(:,iQ) = trapz(DH+AH)*da;    
+            Q_val(:,iQ) = trapz(DH+AH,1)*da;    
         case 'EE-DA-02-10'            
-            Q_val(:,iQ) = trapz(DH(ind0210y)+AH(ind0210y))*da;  
+            Q_val(:,iQ) = trapz(DH(ind0210y,:)+AH(ind0210y,:),1)*da;  
         case 'EE-DA-09-24'
-            Q_val(:,iQ) = trapz(DH(ind0924m)+AH(ind0924m))*da;  
+            Q_val(:,iQ) = trapz(DH(ind0924m,:)+AH(ind0924m,:),1)*da;  
         case 'EE-D-frac'
-            Q_val(:,iQ) = trapz(DH)/trapz(DH+AH);
+            Q_val(:,iQ) = trapz(DH,1)/trapz(DH+AH,1);
         case 'EE-D-frac-02-10'
-            Q_val(:,iQ) = trapz(DH(ind0210y))/trapz(DH(ind0210y)+AH(ind0210y));
+            Q_val(:,iQ) = trapz(DH(ind0210y,:),1)./trapz(DH(ind0210y,:)+AH(ind0210y,:),1);
         case 'EE-D-frac-09-24'
-            Q_val(:,iQ) = trapz(DH(ind0924m))/trapz(DH(ind0924m)+AH(ind0924m));
+            Q_val(:,iQ) = trapz(DH(ind0924m,:),1)./trapz(DH(ind0924m,:)+AH(ind0924m,:),1);
         case 'EE-death' % Cumulative disease-induced mortality, diagnostic eqn MH
-            Q_val(:,iQ) = trapz(MH)*da;
+            Q_val(:,iQ) = trapz(MH,1)*da;
         case 'EE-death-02-10' % Cumulative disease-induced mortality, diagnostic eqn MH
-            Q_val(:,iQ) = trapz(MH(ind0210y))*da;
+            Q_val(:,iQ) = trapz(MH(ind0210y,:),1)*da;
         case 'EE-death-09-24' % Cumulative disease-induced mortality, diagnostic eqn MH
-            Q_val(:,iQ) = trapz(MH(ind0924m))*da;
+            Q_val(:,iQ) = trapz(MH(ind0924m,:),1)*da;
         case 'EE-EIR'
-            NH = trapz(PH)*da;
+            NH = trapz(PH,1)*da;
             NM = SM+EM+IM;
             [bH,~] = biting_rate(NH,NM);
             IM_frac = IM./NM;
             Q_val(:,iQ) = bH.*IM_frac*365; % annual EIR
         case 'EE-Ctot-pp'
-            Q_val(:,iQ) = trapz(Ctot)/trapz(PH);
+            Q_val(:,iQ) = trapz(Ctot,1)./trapz(PH,1);
         case 'EE-Ctot-pp-02-10'
-            Q_val(:,iQ) = trapz(Ctot(ind0210y))/trapz(PH(ind0210y));
+            Q_val(:,iQ) = trapz(Ctot(ind0210y,:),1)./trapz(PH(ind0210y,:),1);
         case 'EE-Ctot-pp-09-24'
-            Q_val(:,iQ) = trapz(Ctot(ind0924m))/trapz(PH(ind0924m));
-        case 'year5-D'
-            tfinal = 5*365;
-            [SH_solu, EH_solu, DH_solu, AH_solu, VH_solu, UH_solu, SM_solu, EM_solu, IM_solu, Cm_solu, Cac_solu, Cv_solu, Ctot_solu, MH_solu] = ...
-                age_structured_Malaria_vac(P.da,P.na,tfinal,SH, EH, DH, AH, VH, UH, SM, EM, IM, Cm, Cac, Cv, Ctot, MH);   
-            DH = DH_solu(:,end);
-            Q_val(:,iQ) = trapz(DH)*da;
+            Q_val(:,iQ) = trapz(Ctot(ind0924m,:),1)./trapz(PH(ind0924m,:),1);
         otherwise
             keyboard
     end
--- run_SA_PRCC.m
+++ run_SA_PRCC.m
@@ -7,8 +7,8 @@ format long
 global P
 
 % numerical config
-tfinal = 20*365; % time for "EE" approximation
-tfinal_vac = 10*365; % time to achieve "new EE" w/ vaccination
+tfinal = 5*365; % time for integration
+tfinal_vac = 0*365; % time to achieve "new EE" w/ vaccination
 age_max = 100*365; % max ages in days
 P.age_max = age_max;
 dt = 20; % time/age step size in days, default = 5;
@@ -19,16 +19,17 @@ a = (0:da:age_max)';
 na = length(a);
 P.a = a; P.na = na; P.nt = nt; P.dt = dt; P.da = da; P.t = t; P.tfinal = tfinal; P.tfinal_vac = tfinal_vac;
 
-% SA setting
-lQ = {'EE-D','EE-DA','EE-D-frac','EE-EIR',...
-    'EE-D-02-10','EE-DA-02-10','EE-D-frac-02-10',...
-    'EE-D-09-24','EE-DA-09-24','EE-D-frac-09-24',...
-    'EE-death','EE-death-02-10','EE-death-09-24'};  
-% lQ = {'EE-death','EE-death-02-10','EE-death-09-24'};  
+% SA setting 
+% lQ = {'EE-D','EE-DA','EE-D-frac','EE-EIR',...
+%     'EE-D-02-10','EE-DA-02-10','EE-D-frac-02-10',...
+%     'EE-D-09-24','EE-DA-09-24','EE-D-frac-09-24',...
+%     'EE-death','EE-death-02-10','EE-death-09-24'};  
+lQ = {'EE-D'};  
 Size_QOI = length(lQ); % length of the QOI. Default = 1, unless it is an age distribution, or wants to test multiple QOIs at once
-time_points = 1; % default time_points = 1, unless if wants to check QOI at particular time points
-% lP_list = { 'dac','cX','w','etas'};
-lP_list = {'rA','rD','muM','sigma','betaM','betaD', 'betaA','dac','cX','phis2','phir2','rhos2','rhor2','psis2','psir2','w','etas'};
+% time_points = length(t); % default # time_points = at tfinal, unless if wants to check QOI at particular time points
+time_points = 1:nt; 
+lP_list = {'rD','rA', 'betaM'};
+% lP_list = {'rA','rD','muM','sigma','betaM','betaD', 'betaA','dac','cX','phis2','phir2','rhos2','rhor2','psis2','psir2','w','etas'};
 % 'rA','muM','sigma','betaM','betaD', 'betaA','dac','cX','phis2','phir2','rhos2','rhor2','psis2','psir2','w'
 lP_list{end+1} = 'dummy'; % add dummy to the POIs
 Malaria_parameters_baseline;
@@ -40,7 +41,7 @@ for iP = 1:length(lP_list)
 end
 
 % PRCC config
-NS = 1000; % number of samples, min = k+1, 100~1000
+NS = 100; % number of samples, min = k+1, 100~1000
 k = length(lP_list); % # of POIs
 % Pre-allocation
 Size_timepts = length(time_points); % # of time points to check QOI value;
@@ -53,39 +54,35 @@ tic
 %% model evaluations
 % common calculations/quantities that don't be impacted by the POIs
 % calculate here once for efficiency
+
 Malaria_parameters_baseline;
+Malaria_parameters_transform_vac;
+P.ss_S = @(t) P.ss_S0*(P.ss_c+P.ss_v*(1-P.ss_c)*((1+cos(2*pi*((t+P.ss_t0)/365-P.ss_u1)))./2).^P.ss_k1+...
+    (1-P.ss_v)*(1-P.ss_c)*((1+cos(2*pi*((t+P.ss_t0)/365-P.ss_u2)))./2).^P.ss_k2);
+P.gM0 = P.gM;
 gH_fun = @(age) (2.*P.cc.*normpdf((age./365-P.zz)./P.ww).*normcdf(P.alpha.*(age./365-P.zz)./P.ww)./P.ww)./365/2;
 gH =  gH_fun(a); % human fertility rate
 muH =  P.b0 + P.b1*exp(-P.b2*a/365) + P.b3*exp(P.b4*a/365); % natural human mortality rate
 muH_fun = @(age) P.b0 + P.b1*exp(-P.b2*age/365) + P.b3*exp(P.b4*age/365);
 muH = muH/365;
 muH_int_fun = @(age) (age./365).*P.b0 + (P.b1./P.b2).*(1-exp(-P.b2.*age./365)) + (P.b3./P.b4).*(-1+exp(P.b4.*age./365));
-muD =  P.b0D + P.b1D*exp(-P.b2D*a/365) + P.b3D*exp(P.b4D*a/365); % natural human mortality rate
-muD_fun = @(age) P.b0D + P.b1D*exp(-P.b2D*age/365) + P.b3D*exp(P.b4D*age/365);
+P.muH_int = muH_int_fun(a);
+
+a74 = 74*365;
+temp_muD = P.b0D + P.b1D*exp(-P.b2D*a74/365) + P.b3D*exp(P.b4D*a74/365);
+muD =  (P.b0D + P.b1D*exp(-P.b2D*a/365) + P.b3D*exp(P.b4D*a/365)).*(a/365<=74)...
+    + temp_muD.*(exp(-P.c0D*(a-74*365)./365)).*(a/365>74); 
+muD_fun = @(age) (P.b0D + P.b1D*exp(-P.b2D*age/365) + P.b3D*exp(P.b4D*age/365)).*(age/365<=74)...
+    + temp_muD.*(exp(-P.c0D*(age/365-74))).*(age/365>74); 
 muD = muD/365;
-muD_int_fun = @(age) (age./365).*P.b0D + (P.b1D./P.b2D).*(1-exp(-P.b2D.*age./365)) + (P.b3D./P.b4D).*(-1+exp(P.b4D.*age./365));
-v_fun = @(age) P.v0.*ones(size(age));
-v = v_fun(a);
 P.muH = muH;
 P.muH_fun = muH_fun;
+P.muH_int_fun = muH_int_fun;
+
 P.muD = muD;
 P.muD_fun = muD_fun;
 P.gH = gH;
-P.v = v;
-P.v_fun = v_fun;
 P.gH_fun = gH_fun;
-P.muH_int_fun = muH_int_fun;
-P.muH_int = muH_int_fun(a);
-P.muD_int_fun = muD_int_fun;
-P.muD_int = muD_int_fun(a);
-% Update the fertility and stable age dist. if balanced option is selected
-if P.balance_fertility == 1
-    balance_fertility;
-end
-
-if P.balance_mortality == 1
-    balance_mortality;
-end
 find_stable_age; % depends on gH and muH
 
 for run_num = 1:NS % Loop through each parameter sample
@@ -109,7 +106,6 @@ for itime = 1:Size_timepts
     end
 end
 % save(['Results/vaccine_no/PRCC_result_',num2str(NS),'_',num2str(k),'.mat'],'PRCC','stat_p','lP_list','lQ')
-
 toc
 
 %% Plotting POIs vs. QOIs: check monotonic relationships
@@ -130,36 +126,54 @@ toc
 % end
 
 %% Sorting (using order from eFAST)
-load(['Results/vaccine_yes/PRCC_result_',num2str(NS),'_',num2str(k),'.mat'],'PRCC','stat_p','lP_list','lQ')
-lP_eFAST = {'rD','cX','dac','psir2','muM','betaM', 'rhor2','betaD','rhos2','rA','sigma','psis2','phir2','phis2','betaA','w','etas'}; 
-[~,index] = ismember(lP_eFAST,lP_list); index = index';
+% load(['Results/vaccine_yes/PRCC_result_',num2str(NS),'_',num2str(k),'.mat'],'PRCC','stat_p','lP_list','lQ')
+% lP_eFAST = {'rD','cX','dac','psir2','muM','betaM', 'rhor2','betaD','rhos2','rA','sigma','psis2','phir2','phis2','betaA','w','etas'}; 
+% [~,index] = ismember(lP_eFAST,lP_list); index = index';
 % [~,index] = sort(abs(PRCC(1:end-1,1,11)),'descend'); % sort using QOI #11, sort all the POIs except dummy
-PRCC = PRCC([index;k],:,:); stat_p = stat_p(index,:,:,:);
-lP_list = lP_list([index;k]);
+% PRCC = PRCC([index;k],:,:); stat_p = stat_p(index,:,:,:);
+% lP_list = lP_list([index;k]);
 
-% PRCC bar plot
+%% PRCC plot 
 palpha = 0.05; % alpha for t-test
-QOI_plot = [2,6,9]; % [11:13] deaths; [2,6,9]: total infected
+QOI_plot = [1]; % [11:13] deaths; [2,6,9]: total infected
 Size_QOI_plot = length(QOI_plot);
-X = categorical(lP_list);
-X = reordercats(X,lP_list);
-for itime = 1:Size_timepts
+if Size_timepts == 1  % bar plot (for one time point)
+    X = categorical(lP_list);
+    X = reordercats(X,lP_list);
     for iQOI = 1:Size_QOI_plot
         figure_setups; hold on
-%         subplot(Size_timepts,Size_QOI_plot,(itime-1)*Size_QOI_plot+iQOI)
-        b = bar(X,PRCC(:,itime,QOI_plot(iQOI)),'FaceAlpha',0.5);
+        %         subplot(Size_timepts,Size_QOI_plot,(itime-1)*Size_QOI_plot+iQOI)
+        b = bar(X,PRCC(:,1,QOI_plot(iQOI)),'FaceAlpha',0.5);
         ylim([-1.1 1.1])
         xtips = b.XEndPoints;
         ytips = b.YEndPoints;
         labels = cell(1, length(lP_list));
-        labels(stat_p(:,itime,QOI_plot(iQOI))<palpha) = {'*'};
-%         for j = 1:k
-%             labels{j} = sprintf('p = %3.1e',stat_p(j,itime,QOI_plot(iQOI)));
-%         end
+        labels(stat_p(:,1,QOI_plot(iQOI))<palpha) = {'*'};
+        %         for j = 1:k
+        %             labels{j} = sprintf('p = %3.1e',stat_p(j,itime,QOI_plot(iQOI)));
+        %         end
         text(xtips,ytips,labels,'HorizontalAlignment','center',...
             'VerticalAlignment','bottom','fontsize',14)
         title(['QOI=', lQ{QOI_plot(iQOI)}])
-        saveas(gcf,['Results/vaccine_yes/PRCC_result_',num2str(NS),'_',num2str(k),'_',lQ{QOI_plot(iQOI)},'.png'])
+        % saveas(gcf,['Results/vaccine_yes/PRCC_result_',num2str(NS),'_',num2str(k),'_',lQ{QOI_plot(iQOI)},'.png'])
+    end
+else
+    QOI_plot = [1]; % [11:13] deaths; [2,6,9]: total infected
+    Size_QOI_plot = length(QOI_plot);
+    for iQOI = 1:Size_QOI_plot   
+        figure_setups; hold on
+        for iPOI = 1:k
+            PRCC_vec = PRCC(iPOI,:,QOI_plot(iQOI))';
+            plot(t(time_points)/365,PRCC_vec)            
+            labels = cell(size(PRCC_vec));
+            labels(stat_p(iPOI,:,QOI_plot(iQOI))'<palpha) = {'*'};
+            text(t(time_points)/365,PRCC_vec,labels,'HorizontalAlignment','center',...
+                'VerticalAlignment','bottom','fontsize',14)
+        end
+        legend(lP_list)
+        xlabel('years')
+        title(['QOI=', lQ{QOI_plot(iQOI)}])
+        % saveas(gcf,['Results/vaccine_yes/PRCC_result_',num2str(NS),'_',num2str(k),'_',lQ{QOI_plot(iQOI)},'.png'])
     end
 end
 
