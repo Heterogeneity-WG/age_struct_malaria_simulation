4de7d1dbef942116017f9aa70929863ea7a0a4c2
Zhuolin Qu <8141673+zhuolinqu@users.noreply.github.com>
2023-06-13 10:27:02 -0500

    Clean up the Data_Fitting codes
    - The main code for fitting is "run_parameter_fit_dynamic2.m"
    - added function descriptions at beginning of the code

--- Data_Fitting/run_parameter_fit_dynamic2.m
+++ Data_Fitting/run_parameter_fit_dynamic2.m
@@ -1,4 +1,8 @@
-% calibration of sigmoids with multiple initial sampling of x0 for fmincon
+% calibration of sigmoids 
+% with multiple initial sampling of x0 for fmincon -> nrun
+% and fixed beta_list values
+% Note: set tfinal = 10 years for IC = EE_reset to cut down CPU time
+
 close all;
 clearvars;
 clc;
@@ -12,42 +16,44 @@ dt = 20; da = dt; t = (0:dt:tfinal)'; nt = length(t); a = (0:da:age_max)'; na = 
 P.a = a; P.na = na; P.nt = nt; P.dt = dt; P.da = da; P.t = t; P.tfinal = tfinal;
 
 %% load data - interpolated function
-s =  load('Filipe_paper/F_Filipe.mat','F');
-F = s.F;
+load('Filipe_paper/F_Filipe.mat','F');
 Malaria_parameters_baseline;
 Malaria_parameters_transform;
 Malaria_parameters_transform_vac;
 np = 6; % # of sigmoid parameters to be fitted
-nrun = 10; % # of LHC samples to generate
+nrun = 1; % # of LHC samples to generate
 
 %% optimization - fit to data 
 lb = [0, 0.1, 0, 0.1, 0, 0.1];
-ub = [5, 4, 5, 4, 5, 4];
+ub = [5, 5, 5, 5, 5, 5];
 
-x_lhsraw = lhsdesign(nrun,np); 
-x_lhs = NaN(size(x_lhsraw));
-for k = 1:np
-    x_lhs(:,k)=(x_lhsraw(:,k).*(ub(k)-lb(k)))+lb(k);
-end
+% x_lhsraw = lhsdesign(nrun,np); 
+% x_lhs = NaN(size(x_lhsraw));
+% for k = 1:np
+%     x_lhs(:,k)=(x_lhsraw(:,k).*(ub(k)-lb(k)))+lb(k);
+% end
 
 % [phi_s phi_r rho_s rho_s psi_r psi_r phif0 phif1 rhof0 rhof1 psif0 psif1]
-% x0 = (lb+ub)/2;
+x0 = (lb+ub)/2;
 
 options = optimset('Display','iter','TolX',10^-5,'MaxIter',30);
 Xmat = NaN(nrun,np+1);
+tic
 for irun = 1:nrun
     disp(['run #',num2str(irun)])
     Malaria_parameters_baseline;
-    x0 = x_lhs(irun,:);
+    % x0 = x_lhs(irun,:);
     [x,fval] = fmincon(@fun_Filipe_dynamic2,x0,[],[], [], [], lb, ub, [], options);
     Xmat(irun,1) = fval;
     Xmat(irun,2:end) = x;
-    save('Data_Fitting/Filipe_paper/Xmat_fine.mat','Xmat','irun','x_lhs')
+    % save('Data_Fitting/Filipe_paper/Xmat_new.mat','Xmat','nrun','x_lhs')
+    save('Data_Fitting/Filipe_paper/Xmat_new.mat','Xmat','nrun','x0')
 end
-keyboard
-%%
-load('Data_Fitting/Filipe_paper/Xmat.mat','Xmat','irun','x_lhs')
-for irun = 1:size(x_lhs,1)
+toc
+%% plot sigmoids and calculate the populational average 
+% load('Data_Fitting/Filipe_paper/Xmat_new.mat','Xmat','nrun','x_lhs')
+load('Data_Fitting/Filipe_paper/Xmat_new.mat','Xmat','nrun','x0')
+for irun = 1:nrun
     x = Xmat(irun,2:end);
     Malaria_parameters_baseline;
     P.phis2 = x(1);
@@ -58,11 +64,10 @@ for irun = 1:size(x_lhs,1)
     P.psir2 = x(6);
     Malaria_parameters_transform;
     Malaria_parameters_transform_vac;
-    [SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0] = age_structured_Malaria_IC_vac('init');
-    [~,SH, EH, DH, AH, ~,~, ~, ~, ~, ~, ~, ~, Ctot, ~] = age_structured_Malaria_vac(P.da,P.na,0,P.tfinal,SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0);
-    PH = SH(:,end)+EH(:,end)+DH(:,end)+AH(:,end);
+    [SH, EH, DH, AH, ~, ~, ~, ~, ~, ~, ~, ~, Ctot, ~] = age_structured_Malaria_IC_vac('EE_reset');
+    PH = SH+EH+DH+AH;
     NH = trapz(PH)*P.da;
-    Ctot_pp = Ctot(:,end)./PH;
+    Ctot_pp = Ctot./PH;
     figure_setups; hold on
     cc = linspace(0,max(Ctot_pp),100);
     phi_curve = sigmoid_prob(cc, 'phi');
@@ -79,79 +84,22 @@ for irun = 1:size(x_lhs,1)
     axis([0 max(cc) 0 1])
     xlabel('$\tilde{C}_{H}$')
     ylabel('Probability')
-    %title('Calibrated linking functions')
+    title('Calibrated linking functions')
     [phi_ave rho_ave psi_ave]
-    fun_Filipe_dynamic2(x);
+    % fun_Filipe_dynamic2(x);
 end
 
 % keyboard
 % ----> update Malaria_parameters_baseline.m with the fitted results <-----
 
-%% plot sigmoids with the populational average (in legend)
-tfinal = 10*365; age_max = 100*365; P.age_max = age_max;
-dt = 20; da = dt; t = (0:dt:tfinal)'; nt = length(t); a = (0:da:age_max)'; na = length(a);
-P.a = a; P.na = na; P.nt = nt; P.dt = dt; P.da = da; P.t = t; P.tfinal = tfinal;
-x = Xmat(55,2:end);
-% [~,ind1] = min(abs(P.a-0.3*365)); 
-% [~,ind0] = min(abs(P.a-3*365)); 
-% [~,ind2] = min(abs(P.a-20*365)); 
-% ind_a = [round(linspace(ind1,ind0,15)'); round(linspace(ind0+1,ind2,nsamp-15)')];
-% x = [1.174354913102552   3.865058149466797   1.551219543369452   0.787630781652653   4.279593577031902   3.994787277041905];
-% fine EIR mesh
-% x = [3.266020807401758   3.248827198899857   1.594701633752857   0.837465044857498   4.664942886501461   4.300057715509009];
-% [~,ind1] = min(abs(P.a-0.3*365)); 
-% [~,ind0] = min(abs(P.a-10*365)); 
-% [~,ind2] = min(abs(P.a-20*365)); 
-% ind_a = [round(linspace(ind1,ind0,20)'); round(linspace(ind0+1,ind2,nsamp-20)')];
-% x = [3.291597293958823   4.243807990790567   1.646220399576347   0.774696249293831   2.119792106093347   4.427487320234270];
-% [~,ind1] = min(abs(P.a-0.3*365)); 
-% [~,ind0] = min(abs(P.a-20*365)); 
-% [~,ind2] = min(abs(P.a-50*365)); 
-% ind_a = [round(linspace(ind1,ind0,20)'); round(linspace(ind0+1,ind2,nsamp-20)')];
-% x = [1.457776066263742   4.097149381426517   1.314712272979270   0.796127082004893   2.874959267261557   4.173818846106313];
-Malaria_parameters_baseline;
-P.phis2 = x(1);
-P.phir2 = x(2); 
-P.rhos2 = x(3);
-P.rhor2 = x(4); 
-P.psis2 = x(5);
-P.psir2 = x(6);
-Malaria_parameters_transform;
-[SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0] = age_structured_Malaria_IC_vac('init');
-[SH, EH, DH, AH, ~, ~, SM, EM, IM, ~, ~, ~, Ctot, ~] = age_structured_Malaria_vac(P.da,P.na,P.tfinal,SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0);
-PH = SH(:,end)+EH(:,end)+DH(:,end)+AH(:,end);
-NH = trapz(PH)*P.da;
-% EIR = fit_EIR(SH,EH,DH,AH,SM, EM, IM);   
-% keyboard
-Ctot_pp = Ctot(:,end)./PH;
-figure_setups; hold on
-cc = linspace(0,max(Ctot_pp),100);
-phi_curve = sigmoid_prob(cc, 'phi');
-rho_curve = sigmoid_prob(cc, 'rho');
-psi_curve = sigmoid_prob(cc, 'psi');
-plot(cc,phi_curve,'-')
-plot(cc,rho_curve,'--')
-plot(cc,psi_curve,'-.')
-% population average sigmoids
-phi_ave = trapz(sigmoid_prob(Ctot_pp, 'phi').*PH/NH)*P.da;
-rho_ave = trapz(sigmoid_prob(Ctot_pp, 'rho').*PH/NH)*P.da;
-psi_ave = trapz(sigmoid_prob(Ctot_pp, 'psi').*PH/NH)*P.da;
-legend('$\phi(\tilde{C}_{H})$','$\rho(\tilde{C}_{H})$','$\psi(\tilde{C}_{H})$','Location','e')
-axis([0 max(cc) 0 1])
-xlabel('$\tilde{C}_{H}$')
-ylabel('Probability')
-%title('Calibrated linking functions')
-[phi_ave rho_ave psi_ave]
-
-
 %% generate data for heatmap (age, EIR, immunity level)
-tfinal = 20*365; age_max = 100*365; P.age_max = age_max;
+tfinal = 10*365; age_max = 100*365; P.age_max = age_max;
 dt = 20; da = dt; t = (0:dt:tfinal)'; nt = length(t); a = (0:da:age_max)'; na = length(a);
 P.a = a; P.na = na; P.nt = nt; P.dt = dt;    P.da = da; P.t = t; P.tfinal = tfinal;
 
 Malaria_parameters_baseline;
 Malaria_parameters_transform;
-% Malaria_parameters_transform_vac;
+Malaria_parameters_transform_vac;
 P.phis2 = x(1);
 P.phir2 = x(2); 
 P.rhos2 = x(3);
@@ -182,56 +130,38 @@ zz = zeros(na,length(var_list));
 for jj = 1:length(var_list)
     P.betaM = var_list(jj);
     Malaria_parameters_transform;
-    [SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0] = age_structured_Malaria_IC_vac('init');
-    [SH, EH, DH, AH, ~, ~, SM, EM, IM, ~, ~, ~, Ctot, ~] = age_structured_Malaria_vac(P.da,P.na,P.tfinal,SH0, EH0, DH0, AH0, VH0, UH0, SM0, EM0, IM0, Cm0, Cac0, Cv0, Ctot0, MH0);
-    EIR = fit_EIR(SH,EH,DH,AH,SM, EM, IM);   
-    PH = SH+EH+DH+AH;    
-    yy(1,jj) = EIR(end); % aEIR
-    xxs(:,jj) = xx;
-    yys(:,jj) = EIR(end)*ones(size(yys(:,jj)));
-    zz(:,jj) = Ctot(:,end)./PH(:,end); % final Ctot at EE    
+    [SH, EH, DH, AH, ~, ~, SM, EM, IM, ~, ~, ~, Ctot, ~] = age_structured_Malaria_IC_vac('EE_reset');
+    PH = SH+EH+DH+AH;
+    NM = SM+EM+IM;
+    [bH,~] = biting_rate(PH,NM);
+    EIR = bH.*IM./NM*365; % EIR matrix
+    EIR_tot = trapz(EIR.*PH)/trapz(PH); % EIR sum over age, at final time
+    yy(1,jj) = EIR_tot; % aEIR
+    zz(:,jj) = Ctot./PH; % final Ctot at EE    
 end
-figure_setups;
-plot(var_list,yy)
-%% sorting
-% [yy,ind] = sort(yy);
-% zz = zz(:,ind);
 %% plotting heatmap (age, EIR, immunity) 
-figure_setups; imagesc(xx,yy,zz')
-% plot(var_list,y,'-o');
-figure_setups; grid off; [XX,YY] = meshgrid(xx,yy); surf(XX,YY,zz'); shading interp; view(2)
-figure_setups; grid off; scatter3(xxs(:),yys(:),zz(:))
-axis([0 20 0 190])
+figure_setups;
+imagesc(xx,yy,zz')
+xlim([0 20])
+ylim([0 max(yy)])
 xlabel('Age (years)')
 ylabel('aEIR')
 title('Immunity level per person');
 set(gca,'YDir','normal');
 colormap jet
-caxis([0 12])
-colorbar
-zz_rho = sigmoid_prob(zz, 'rho');
-%% plotting heatmap (age, EIR, rho) -> to compare with Filipe's curves
+colorbar('Ticks',0:2:15);
+%% plotting heatmap (age, EIR, rho)
 figure_setups; hold on; grid off
 zz_rho = sigmoid_prob(zz, 'rho');
-% zz_psi = sigmoid_prob(zz, 'psi');
-% imagesc(xx,yy,zz_rho')
-[XX,YY] = meshgrid(xx,yy);
-surf(xx,yy,zz_rho')
-shading interp
-axis([0 20 0 190])
-view(2)
+imagesc(xx,yy,zz_rho')
 xlabel('Age (years)')
 ylabel('aEIR')
-% title(['Immunity levels, feedback = ',num2str(immunity_feedback)]);
 title('Susceptibility ($\rho$)');
 set(gca,'YDir','normal');
 colormap jet
 colorbar('Ticks',0:0.2:1);
+xlim([0 20])
 caxis([0 max(max(zz_rho))])
-% EIR_list = [1 10 20 50 100];
-% for iEIR = 1:length(EIR_list)
-%     plot(xx,EIR_list(iEIR)*ones(size(xx)),'m--')
-% end
 %% slices of heatmap to compare with Filipe's curves
 EIR_list = [1 10 20 50 100];
 figure_setups; hold on
@@ -251,13 +181,3 @@ legend('Location','e')
 xlabel('age (years)')
 ylabel('susceptibility')
 axis([0 50 0 1.05 ])
-
-
-function EIR = fit_EIR(SH,EH,DH,AH,SM,EM,IM)
-global P
-NH = trapz(SH+EH+DH+AH)*P.da;
-NM = SM+EM+IM;
-[bH,~] = biting_rate(NH,NM);
-IM_frac = IM./NM;
-EIR = bH.*IM_frac*365; % annual EIR
-end
